b17b219dc0d221c1bda0b12d76730f892ed5899a
léni <lenipep@hotmail.fr>
2019-09-10 09:48:57 +0200

    init ntm je compte plus batard

---
+++ exercice 1/script2.js
@@ -0,0 +1,10 @@
+//exercice 1
+
+
+var image = document.getElementById("image1") ;
+
+
+
+image.addEventListener("mouseover", function(){    
+    image.src = "/home/leni/code/js-exo-base-2/exercice 1/images/image1_2.jpg"
+});
