45fbbe8eafcf35f490021bef13a58e0ad2e38277
coudert <jean.baptiste.coudert1@gmail.com>
2019-09-10 12:23:16 +0200

    fin base 2

--- exercice 6/script2_6.js
+++ exercice 6/script2_6.js
@@ -1,4 +1,3 @@
-
 var myImage1 = document.getElementById("image1");
 var myImage2 = document.getElementById("image2");
 var myImage3 = document.getElementById("image3");
@@ -9,10 +8,18 @@ var myImage5 = document.getElementById("image5");
 var allImages = document.getElementsByTagName("img")
 
 
+
 for (let i = 0; i < allImages.length; i++) {
     allImages[i].addEventListener("mouseover", function () {
         for (let j = 0; j < allImages.length; j++){
             allImages[j].src = "images/image" + (j+1) + "_2.jpg";
         }
     })
+};
+for (let i = 0; i < allImages.length; i++) {
+    allImages[i].addEventListener("mouseleave", function () {
+        for (let j = 0; j < allImages.length; j++) {
+        allImages[j].src = "images/image" + (j+1) + ".jpg";
+        }
+    });
 };
